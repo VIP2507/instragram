6d08a1ea79db73375d3502af6d5a7e0ef5f05a81
VIPULSINH <73651853+VIP2507@users.noreply.github.com>
2023-09-24 13:23:08 +0000

    firbase update

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,18 @@
 import firebase from "firebase";
 
 var firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyBYfFdtRFdaxgZCXjfgxzbE_JwkaxVnsUM",
-  authDomain: "insta-a7137.firebaseapp.com",
-  projectId: "insta-a7137",
-  storageBucket: "insta-a7137.appspot.com",
-  messagingSenderId: "214576447464",
-  appId: "1:214576447464:web:788a17f6630b86db07ee5d",
-  measurementId: "G-STG1DZBMQK"
-
+  apiKey: "AIzaSyBWRgVYqudNRmURJyG0NgQb7Yu6YXcjI7Q",
+  authDomain: "instagram-731f8.firebaseapp.com",
+  projectId: "instagram-731f8",
+  storageBucket: "instagram-731f8.appspot.com",
+  messagingSenderId: "929077661846",
+  appId: "1:929077661846:web:d1e81cfb4e753969a6f2b7",
+  measurementId: "G-PGCX5TBH75"
 });
 
 var db = firebaseApp.firestore();
-var auth=firebaseApp.auth();
-var storage=firebaseApp.storage()
+var auth = firebaseApp.auth();
+var storage = firebaseApp.storage()
 
 
-export { db ,auth,storage};
+export { db, auth, storage };
